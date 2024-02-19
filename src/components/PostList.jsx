72ef6a4c9afafb558fc21f6be24f790a3ce2439c
saurav-ghimire <sauravghimire0123@gmail.com>
2024-02-19 15:15:27 -0500

    for data fetching we replaced the setState with useEffect Hook

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect} from "react";
 import Post from "./Post";
 import {postList as postListData} from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
@@ -7,16 +7,14 @@ import WelcomeMessage from "./WelcomeMessage";
 function PostList() {
   
   const {PostLists, addInitialPosts} = useContext(postListData);
-
-  const [fetchedData, setFetchedData] = useState(false);
-  if(!fetchedData){
+  
+  useEffect(() => {
     fetch('https://dummyjson.com/posts')
     .then(res => res.json())
     .then(data => {
       addInitialPosts(data.posts)
-    });
-    setFetchedData(true)
-  }
+    })
+  }, [])
 
   return <>
         {PostLists.length === 0 && <WelcomeMessage />}
