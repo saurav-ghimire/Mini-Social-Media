3a466dc1a3530d0c8ea7e1856a13e24e7775660b
saurav-ghimire <sauravghimire0123@gmail.com>
2024-02-19 15:35:37 -0500

    Handling Loading State using the setState

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,29 @@
-import { useContext, useEffect} from "react";
+import { useContext, useEffect, useState} from "react";
 import Post from "./Post";
 import {postList as postListData} from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
-
+import LoadingSpinner from "./LoadingSpinner";
 
 function PostList() {
+  const[fetching, setFetching] = useState(false)
   
   const {PostLists, addInitialPosts} = useContext(postListData);
   
   useEffect(() => {
+    setFetching(true)
     fetch('https://dummyjson.com/posts')
     .then(res => res.json())
     .then(data => {
       addInitialPosts(data.posts)
+      setFetching(false)
     })
+    
   }, [])
 
   return <>
-        {PostLists.length === 0 && <WelcomeMessage />}
-        {PostLists.map((data) => <Post key={data.id} post={data} />)}
+        {fetching && <LoadingSpinner/>}
+        {!fetching && PostLists.length === 0 && <WelcomeMessage />}
+        {!fetching && PostLists.map((data) => <Post key={data.id} post={data} />)}
     </>
     ;
 }
